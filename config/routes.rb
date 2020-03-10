3742ac5b2396c055b2e5f046f446c8777a2530e5
Yung Cher Ho <ycho527@hotmail.com>
2020-03-10 11:08:33 +0800

    Migrated database and created model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/edit'
+  get 'tasks/update'
+  get 'tasks/destroy'
+  get 'index/show'
+  get 'index/edit'
+  get 'index/update'
+  get 'index/new'
+  get 'index/create'
+  get 'index/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
